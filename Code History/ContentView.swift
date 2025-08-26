e52999450edfd6bc5cfe05c58ddbbecf436b9910
Stefano Calò <107405736+Stefanocalo@users.noreply.github.com>
2025-08-26 19:30:45 +0200

    Refreshing committing process in xCode

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -8,22 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, friends!")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundColor(Color.blue)
-                .multilineTextAlignment(.center)
-                .padding(.bottom)
-                
-                
-            Text("I am a new app.")
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack{
+                Text("1/10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .bold()
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack{
+                    
+                    Button(action: {
+                        print("Tapped on item 1")
+                    }, label: {Text("Ant")} )
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .border(accentColor, width: 4)
+                    
+                    Button(action: {
+                        print("Tapped on item 2")
+                    }, label: {Text("Beetle")})
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .border(accentColor, width: 4)
+                    
+                    Button(action: {
+                        print("Tapped on item 3")
+                    }, label: {Text("Moth")})
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .border(accentColor, width: 4)
+                    
+                    Button(action: {
+                        print("Tapped on item 4")
+                    }, label: {Text("Fly")})
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .border(accentColor, width: 4)
+                    
+                }
+            }
+            .foregroundColor(Color.white)
         }
-        .padding()
+            
     }
 }
 
